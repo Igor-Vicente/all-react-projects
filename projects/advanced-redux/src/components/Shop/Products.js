b731ec781b519b26ce84710ee54b085be6a1dc5f
igor vicente <igor_machado05@hotmail.com>
2024-05-24 09:44:36 -0300

    redux-advanced

---
+++ projects/advanced-redux/src/components/Shop/Products.js
@@ -0,0 +1,38 @@
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 6,
+    title: "Mars Our New Home",
+    description: "the first book of Mars",
+  },
+  {
+    id: "p2",
+    price: 5,
+    title: "My Favorite Evil",
+    description: "The right balance of comedy and horror",
+  },
+];
+
+const Products = (props) => {
+  return (
+    <section className={classes.products}>
+      <h2>Buy your favorite products</h2>
+      <ul>
+        {DUMMY_PRODUCTS.map((p) => (
+          <ProductItem
+            key={p.id}
+            id={p.id}
+            title={p.title}
+            price={p.price}
+            description={p.description}
+          />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
---
+++ projects/advanced-redux/src/store/ui-slice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const uiSlice = createSlice({
+  name: "ui",
+  initialState: { showCart: false, notification: null },
+  reducers: {
+    toggle(state) {
+      state.showCart = !state.showCart;
+    },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
+    },
+  },
+});
+
+export const uiActions = uiSlice.actions;
